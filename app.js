96761b32f3f99a9b640d958663092a9e702a415f
amysujunglee <amy.sujunglee@gmail.com>
2020-11-04 23:19:46 -0800

    displaying all the harry porter characters

---
+++ app.js
@@ -0,0 +1,28 @@
+const charactersList = document.querySelector('#charactersList');
+console.log(charactersList);
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('http://hp-api.herokuapp.com/api/characters');
+        let hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+        console.log(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters.map((character) => {
+        return `
+        <li class="character">
+            <h2>${character.name}</h2>
+            <p>House: ${character.house}</p>
+            <img src="${character.image}"></img>
+        </li>
+        `;
+    }).join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
